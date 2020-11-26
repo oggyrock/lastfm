0a0bc679b42ed85161c8ac1a3dde4ca2e200ba73
oggyrock <steelo.sun@gmail.com>
2020-11-26 18:00:17 +0200

    simple solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Route } from 'react-router';
+import { HashRouter } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <HashRouter>
+        <div className="App">
+            <Header />
+          <div className='wrapper'>
+            <Route path='/home' component={Home} />
+            <Route path='/search' component={Search} />
+          </div>
+        </div>
+      </HashRouter>
   );
 }
 
